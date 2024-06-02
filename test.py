8ce4712171a35bf08d637e5fb93542a854a6126c
Valentin <56231226+vsnenashev@users.noreply.github.com>
2024-06-02 20:49:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-#ins = (inv(k_global).dot(g_global)).toarray()
-#print(la.eigvals(ins))
-#insa = (inv(g_global).dot(k_global)).toarray()
-#print(la.eigvals(insa))
-#insi = k_global.dot(inv(g_global)).toarray()
-#print(la.eigvals(insi))
-#print(lam_i)
 import pandas as pd
 import ezdxf as ez
 from ezdxf.addons import iterdxf
